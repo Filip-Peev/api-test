db0b1dc4a1f3ea163ec97d36ef8067cb322e01cd
Filip-Peev <Filip.Peev.Official@gmail.com>
2024-10-19 11:12:26 +0300

    Create config.sample.php

---
+++ php-api-mysql/config.sample.php
@@ -0,0 +1,9 @@
+<?php
+// config.php
+
+$config = [
+    'servername' => 'localhost',
+    'username' => 'root', // Replace with your MySQL user
+    'password' => 'your_password_here', // Replace with your MySQL password
+    'dbname' => 'api_example'
+];
